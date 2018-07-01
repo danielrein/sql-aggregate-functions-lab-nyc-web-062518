b225ffac3b449888e1730f477821fa1b2be4df69
Daniel Ran <danielr1735@gmail.com>
2018-07-01 20:28:21 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,10 +9,11 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  'select avg(gpa) from students' 
+  'select avg(gpa) from students'
 end
 
 def total_tardies_for_all_students
+  'select sum(gpa) from students' 
 end
 
 def average_gpa_for_9th_grade
